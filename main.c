d863776b86e57f5e89916adc81294679925a62be
AM429 <ZiyadMatt@gmail.com>
2020-06-26 20:42:14 +0200

    minor Renaming

--- main.c
+++ main.c
@@ -3,16 +3,5 @@
 #include "Headers/Stack.h"
 
 int main() {
-    Stack  *s = StackInitalization(sizeof(float));
-    float *p,j=0.5;
-    p=&j;
-    for(int i=0;i<=50;i++,j=i){
-        pushStack(s,p);
-    }
 
-    while(!isEmptyStack(s)){
-        printf("%f\n",*(float*)popStack(s));
-    }
-    StackClear(s);
-    StackDestroy(s);
 }
