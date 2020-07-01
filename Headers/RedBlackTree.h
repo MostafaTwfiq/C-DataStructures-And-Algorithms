b9f77c9de7053879faec40e7c0dbcf262d1fab3f
AM429 <ZiyadMatt@gmail.com>
2020-07-01 03:54:18 +0200

    Created Red-Black Tree but not implemented

--- Headers/RedBlackTree.h
+++ Headers/RedBlackTree.h
@@ -1,8 +1,27 @@
-//
-// Created by polit on 6/30/2020.
-//
-
 #ifndef C_DATASTRUCTURES_REDBLACKTREE_H
 #define C_DATASTRUCTURES_REDBLACKTREE_H
+#include "Utils.h"
+
+typedef enum COLOR {
+    RED,
+    BLACK
+}COLOR;
+
+typedef struct  RBNode {
+    void * key;
+    COLOR color;
+    struct RBNode *left;
+    struct RBNode *right;
+    struct RBNode *parent;
+}RBNode;
+
+
+typedef struct RBTree{
+    RBNode * root;
+    uint32_t nodeCount;
+    int16_t (*cmp)(const void*,const void *);
+}RBTree;
+
+RBNode* RBTreeInsert(RBTree *pRBTree,void *key);
 
 #endif //C_DATASTRUCTURES_REDBLACKTREE_H
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # C-Data-structures
-![total files](https://img.shields.io/badge/total%20files-38-%23607d8b.svg)
-![total](https://img.shields.io/badge/total%20lines-3625-%232196f3.svg)
+![total files](https://img.shields.io/badge/total%20files-44-%23607d8b.svg)
+![total](https://img.shields.io/badge/total%20lines-3982-%232196f3.svg)
