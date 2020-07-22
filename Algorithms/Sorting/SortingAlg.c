39fa14b3b4e6e69e50de2a1d13b6b92d6d6067ae
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2020-07-22 22:11:30 +0200

    Implemented merge sort.

--- Algorithms/Sorting/SortingAlg.c
+++ Algorithms/Sorting/SortingAlg.c
@@ -5,6 +5,8 @@
 
 void swap(void *first, void *second, int elemSize);
 
+void mergeSortHelper(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
+
 
 
 
@@ -77,19 +79,19 @@ void selectionSort(void *arr, int length, int elemSize, int (*cmp)(const void *,
         exit(INVALID_ARG);
     }
 
-    void *minElement = NULL;
+    void *currentElement = NULL;
 
     for (int i = 0; i < length; i++) {
-        minElement = arr + i * elemSize;
+        currentElement = arr + i * elemSize; // the min or max element depends on if the sorting descending order or not
         for (int j = i; j < length; j++) {
 
-            if ( cmp( minElement, arr + j * elemSize ) > 0 )
-                minElement = arr + j * elemSize;
+            if ( cmp(currentElement, arr + j * elemSize ) > 0 )
+                currentElement = arr + j * elemSize;
 
         }
 
-        if (minElement != arr + i * elemSize)
-            swap(arr + i * elemSize, minElement, elemSize);
+        if (currentElement != arr + i * elemSize)
+            swap(arr + i * elemSize, currentElement, elemSize);
 
     }
 }
@@ -123,6 +125,7 @@ void insertionSort(void *arr, int length, int elemSize, int (*cmp)(const void *,
         memcpy(currentElement, arr + i * elemSize, elemSize);
         int rightIndex = 0;
         for (int j = 0; j < i; j++) {
+
             if ( cmp(currentElement, arr + j * elemSize) <= 0) {
                 rightIndex = j;
                 break;
@@ -144,6 +147,55 @@ void insertionSort(void *arr, int length, int elemSize, int (*cmp)(const void *,
 
 
 
+
+
+void mergeSort(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *)) {
+
+    if (arr == NULL) {
+        fprintf(stderr, NULL_POINTER_MESSAGE, "passed array", "selection sort");
+        exit(NULL_POINTER);
+    } else if (cmp == NULL) {
+        fprintf(stderr, INVALID_ARG_MESSAGE, "comparator function pointer", "selection sort");
+        exit(INVALID_ARG);
+    } else if (length < 0) {
+        fprintf(stderr, INVALID_ARG_MESSAGE, "array length", "selection sort");
+        exit(INVALID_ARG);
+    } else if (elemSize <= 0) {
+        fprintf(stderr, INVALID_ARG_MESSAGE, "element size", "selection sort");
+        exit(INVALID_ARG);
+    }
+
+    mergeSortHelper(arr, length, elemSize, cmp);
+
+}
+
+
+void mergeSortHelper(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *)) {
+    if (length <= 0)
+        return;
+
+    int middleLength = (int) (length / 2);
+    int secondHalfLength = length - middleLength == length ? 0 : length - middleLength;
+
+    mergeSortHelper(arr, middleLength, elemSize, cmp);
+    mergeSortHelper(arr + middleLength * elemSize, secondHalfLength, elemSize, cmp);
+
+    //bubbleSort(arr, length, elemSize, cmp);
+
+    for (int i = middleLength - 1; i >= 0; i--) {
+
+        for (int j = i; j < length - 1; j++) {
+            if ( cmp(arr + j * elemSize, arr + (j + 1) * elemSize ) > 0 )
+                swap(arr + j * elemSize, arr + (j + 1) * elemSize, elemSize);
+            else
+                break;
+
+
+        }
+
+    }
+
+}
 
 
 
--- Algorithms/Sorting/SortingAlg.h
+++ Algorithms/Sorting/SortingAlg.h
@@ -9,4 +9,6 @@ void selectionSort(void *arr, int length, int elemSize, int (*cmp)(const void *,
 
 void insertionSort(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
 
+void mergeSort(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
+
 #endif //C_DATASTRUCTURES_SORTINGALG_H
