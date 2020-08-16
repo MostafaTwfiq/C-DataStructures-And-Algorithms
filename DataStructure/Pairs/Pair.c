94f9426e8f8b7addf57d29d28abb709f4cb10a08
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2020-08-16 20:43:34 +0200

    Added terminating errors to the pair data structure.

--- DataStructure/Pairs/Pair.c
+++ DataStructure/Pairs/Pair.c
@@ -9,18 +9,43 @@
 Pair *pairInitialization(void *fElem, void *sElem, void (*fFreeFn)(void *), void (*sFreeFn)(void *)) {
 
     if (fElem == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return NULL;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "first element pointer", "pair data structure");
+        exit(INVALID_ARG);
+#endif
     } else if (sElem == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return NULL;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "second element pointer", "pair data structure");
+        exit(INVALID_ARG);
+#endif
     } else if (fFreeFn == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return NULL;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "first element free function pointer", "pair data structure");
+        exit(INVALID_ARG);
+#endif
     } else if (sFreeFn == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return NULL;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "second element free function pointer", "pair data structure");
+        exit(INVALID_ARG);
+#endif
     }
 
     Pair *pair = (Pair *) malloc(sizeof(Pair));
     if (pair == NULL) {
-
+        fprintf(stderr, FAILED_ALLOCATION_MESSAGE, "pair", "pair data structure");
+        exit(FAILED_ALLOCATION);
     }
 
     pair->fElem = fElem;
@@ -38,7 +63,13 @@ Pair *pairInitialization(void *fElem, void *sElem, void (*fFreeFn)(void *), void
 void *pairGetFElem(Pair *pair) {
 
     if (pair == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return NULL;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "pair", "pair data structure");
+        exit(NULL_POINTER);
+#endif
     }
 
     return pair->fElem;
@@ -49,7 +80,13 @@ void *pairGetFElem(Pair *pair) {
 void *pairGetSElem(Pair *pair) {
 
     if (pair == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return NULL;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "pair", "pair data structure");
+        exit(NULL_POINTER);
+#endif
     }
 
     return pair->sElem;
@@ -60,7 +97,21 @@ void *pairGetSElem(Pair *pair) {
 void pairSetFElem(Pair *pair, void *newElem) {
 
     if (pair == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "pair", "pair data structure");
+        exit(NULL_POINTER);
+#endif
+    } else if (newElem == NULL) {
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "new element", "pair data structure");
+        exit(INVALID_ARG);
+#endif
     }
 
     pair->fFreeFn(pair->fElem);
@@ -73,7 +124,21 @@ void pairSetFElem(Pair *pair, void *newElem) {
 void pairSetSElem(Pair *pair, void *newElem) {
 
     if (pair == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "pair", "pair data structure");
+        exit(NULL_POINTER);
+#endif
+    } else if (newElem == NULL) {
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "new element", "pair data structure");
+        exit(INVALID_ARG);
+#endif
     }
 
     pair->sFreeFn(pair->sElem);
@@ -87,7 +152,13 @@ void pairSetSElem(Pair *pair, void *newElem) {
 void pairSwapElements(Pair *pair) {
 
     if (pair == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "pair", "pair data structure");
+        exit(NULL_POINTER);
+#endif
     }
 
     void *tempElem = pair->fElem;
@@ -104,7 +175,13 @@ void pairSwapElements(Pair *pair) {
 void destroyPair(Pair *pair) {
 
     if (pair == NULL) {
-
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "pair", "pair data structure");
+        exit(NULL_POINTER);
+#endif
     }
 
     pair->fFreeFn(pair->fElem);
