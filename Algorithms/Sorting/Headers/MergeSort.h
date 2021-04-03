846420c386d36fd41507572b3196499b06d06dbe
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2021-04-03 21:21:55 +0200

    Implemented merge sort with space complexity O(n) and best time complexity O(nlog(n)).

--- Algorithms/Sorting/Headers/MergeSort.h
+++ Algorithms/Sorting/Headers/MergeSort.h
@@ -2,7 +2,7 @@
 #define C_DATASTRUCTURES_MERGESORT_H
 
 
+void mergeSortWS(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
 void mergeSort(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
-
 
 #endif //C_DATASTRUCTURES_MERGESORT_H
--- Algorithms/Sorting/Sources/MergeSort.c
+++ Algorithms/Sorting/Sources/MergeSort.c
@@ -4,6 +4,7 @@
 #include "../../../Unit Test/CuTest/CuTest.h"
 
 
+void mergeSortWSHelper(char *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
 void mergeSortHelper(char *arr, int length, int elemSize, int (*cmp)(const void *, const void *));
 
 
@@ -11,7 +12,60 @@ void mergeSortHelper(char *arr, int length, int elemSize, int (*cmp)(const void 
  *
  * Time Complexity: worst: O( n^2 ) , best: O ( n log(n) ).
  *
- * Space Complexity: O ( 1 ) <<because the recursive calls>>
+ * Space Complexity: O ( 1 )
+ *
+ * @param arr the array pointer
+ * @param length the length of the array
+ * @param elemSize the size of the array elements in bytes
+ * @param cmp the comparator function pointer
+ */
+
+void mergeSortWS(void *arr, int length, int elemSize, int (*cmp)(const void *, const void *)) {
+
+    if (arr == NULL) {
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = NULL_POINTER;
+        return;
+#else
+        fprintf(stderr, NULL_POINTER_MESSAGE, "passed array", "merge sort");
+        exit(NULL_POINTER);
+#endif
+    } else if (cmp == NULL) {
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "comparator function pointer", "merge sort");
+        exit(INVALID_ARG);
+#endif
+    } else if (length < 0) {
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "array length", "merge sort");
+        exit(INVALID_ARG);
+#endif
+    } else if (elemSize <= 0) {
+#ifdef C_DATASTRUCTURES_ERRORSTESTSTRUCT_H
+        ERROR_TEST->errorCode = INVALID_ARG;
+        return;
+#else
+        fprintf(stderr, INVALID_ARG_MESSAGE, "element size", "merge sort");
+        exit(INVALID_ARG);
+#endif
+    }
+
+    mergeSortWSHelper((char *) arr, length, elemSize, cmp);
+
+}
+
+
+/** This function will take an array then it sort it with the merge sort algorithm.
+ *
+ * Time Complexity: worst: O( n log(n) ) , best: O ( n log(n) ).
+ *
+ * Space Complexity: O ( n )
  *
  * @param arr the array pointer
  * @param length the length of the array
@@ -66,7 +120,48 @@ void mergeSort(void *arr, int length, int elemSize, int (*cmp)(const void *, con
  *
  * Time Complexity: worst: O( n^2 ) , best: O ( n log(n) ).
  *
- * Space Complexity: O ( 1 ) <<because the recursive calls>>
+ * Space Complexity: O ( 1 )
+ *
+ * @param arr the array pointer
+ * @param length the length of the array
+ * @param elemSize the size of the array elements in bytes
+ * @param cmp the comparator function pointer
+ */
+
+void mergeSortWSHelper(char *arr, int length, int elemSize, int (*cmp)(const void *, const void *)) {
+    if (length <= 0)
+        return;
+
+
+    int firstHalfLength = (int) (length / 2);
+    int secondHalfLength = length - firstHalfLength == length ? 0 : length - firstHalfLength;
+
+    mergeSortWSHelper(arr, firstHalfLength, elemSize, cmp);
+    mergeSortWSHelper(arr + firstHalfLength * elemSize, secondHalfLength, elemSize, cmp);
+
+    for (int i = firstHalfLength - 1; i >= 0; i--) {
+
+        for (int j = i; j < length - 1; j++) {
+            if (cmp(arr + j * elemSize, arr + (j + 1) * elemSize) > 0)
+                swap(arr + j * elemSize, arr + (j + 1) * elemSize, elemSize);
+            else
+                break;
+
+
+        }
+
+    }
+
+}
+
+
+/** This function will take an array then it sort it recursively with the merge sort algorithm.
+ *
+ * Note: this function should only be called from the mergeSort function.
+ *
+ * Time Complexity: worst: O( n log(n) ) , best: O ( n log(n) ).
+ *
+ * Space Complexity: O ( n )
  *
  * @param arr the array pointer
  * @param length the length of the array
@@ -79,23 +174,32 @@ void mergeSortHelper(char *arr, int length, int elemSize, int (*cmp)(const void 
         return;
 
 
-    int middleLength = (int) (length / 2);
-    int secondHalfLength = length - middleLength == length ? 0 : length - middleLength;
+    int firstHalfLength = (int) (length / 2);
+    int secondHalfLength = length - firstHalfLength == length ? 0 : length - firstHalfLength;
 
-    mergeSortHelper(arr, middleLength, elemSize, cmp);
-    mergeSortHelper(arr + middleLength * elemSize, secondHalfLength, elemSize, cmp);
+    mergeSortHelper(arr, firstHalfLength, elemSize, cmp);
+    mergeSortHelper(arr + firstHalfLength * elemSize, secondHalfLength, elemSize, cmp);
 
-    for (int i = middleLength - 1; i >= 0; i--) {
+    char *tempArr = (char *) malloc(length * elemSize);
+    int counter = 0;
+    short firstHalfCopiedBool = 0;
+    for (int i = firstHalfLength; i < length; i++) {
 
-        for (int j = i; j < length - 1; j++) {
-            if (cmp(arr + j * elemSize, arr + (j + 1) * elemSize) > 0)
-                swap(arr + j * elemSize, arr + (j + 1) * elemSize, elemSize);
-            else
-                break;
-
-
+        if (!firstHalfCopiedBool  && cmp(arr + i * elemSize, arr) > 0) {
+            for (int j = 0; j < firstHalfLength; j++) {
+                memcpy(tempArr + counter++ * elemSize, arr + j * elemSize, elemSize);
+            }
+            firstHalfCopiedBool = 1;
         }
 
+        memcpy(tempArr + counter++ * elemSize, arr + i * elemSize, elemSize);
+
     }
+
+    for (int i = 0; i < length; i++) {
+        memcpy(arr + i * elemSize , tempArr + i * elemSize, elemSize);
+    }
+
+    free(tempArr);
 
 }
